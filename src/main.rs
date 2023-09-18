725e299dd2569f9692a0f0fe6edeec57af3e679c
Mayke Erick <maykeesa.contato@gmail.com>
2023-09-18 17:04:51 -0300

    feat: Aprendendo mais sobre funções

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
-fn main(){
+const PI:f32 = 3.141592653;
+static mut GLOBAL:u8 = 255;
 
+fn main(){
+    println!("| Rust |\n");
+    
+    let result_hello_world:String = hello_world("");
+    println!("{}", result_hello_world);
+    
+    types();
+    
+    let result_soma:String = soma(10, 20);
+    println!("{}", result_soma);
+}
+
+pub fn types(){
+    println!("| Types |\n");
     let variavel:i32 = 100;
     let bytes_var = std::mem::size_of_val(&variavel);
-    println!("| Iniciando no Rust |\n");
-    println!("1 Byte = 8 Bits");
-    println!("{} | {} Bytes | {} Bits", variavel, bytes_var, bytes_var * 8);
-
+    println!("i32 | {} | {} Bytes | {} Bits", variavel, bytes_var, bytes_var * 8);
+    
     let decimal:f32 = 2.5;
     let bytes_decimal = std::mem::size_of_val(&decimal);
-    println!("{} | {} Bytes | {} Bits", decimal, bytes_decimal, bytes_decimal * 8);
-
-    let mut booleano:bool = true;
-    booleano = false;
+    println!("f32 | {} | {} Bytes | {} Bits", decimal, bytes_decimal, bytes_decimal * 8);
+    
+    let booleano:bool = true;
     let bytes_booleano = std::mem::size_of_val(&booleano);
-    println!("{} | {} Bytes", booleano, bytes_booleano);
-
+    println!("bool | {} | {} Bytes", booleano, bytes_booleano);
+    
     let letra:char = 'a';
     let bytes_letra = std::mem::size_of_val(&letra);
-    print!("{} | {} Bytes | {} bits", letra, bytes_letra, bytes_letra * 8);
+    println!("char | {} | {} Bytes | {} bits\n", letra, bytes_letra, bytes_letra * 8);
+    
+    println!("PI = {}\n", PI);
+    unsafe{
+        println!("Global = {}", GLOBAL);
+    }
+}
+
+pub fn hello_world(nome:&str) -> String{
+    println!("| Hi! |\n");
+    if !nome.is_empty() {
+        return format!("Hello World, {}!\n", nome);
+    }
+    
+    format!("Hello World!\n")
+}
+
+pub fn soma(x:i128, y:i128) -> String {
+    println!("| Soma |\n");
+    format!("{} + {} = {}", x, y, x + y)
 }
