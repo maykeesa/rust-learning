2adcb4a92973b68d5c149878920c3720c669921a
Mayke Erick <maykeesa.contato@gmail.com>
2024-02-29 23:35:41 -0300

    feat: Laços de repetição

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,114 @@
-const PI:f32 = 3.141592653;
+//const PI:f32 = 3.141592653;
 static mut GLOBAL:u8 = 255;
 
 fn main(){
     println!("| Rust |\n");
-    
-    let result_hello_world:String = hello_world("");
+    let x:i128 = 4;
+    let y:i128 = 4;
+
+    let result_hello_world:String = hello_world("Mayke");
     println!("{}", result_hello_world);
-    
-    types();
-    
-    let result_soma:String = soma(10, 20);
-    println!("{}", result_soma);
+
+    //types();
+
+    let soma:i128 = soma(x, y);
+    println!("{} + {} = {}", x, y, soma);
+
+    let mult:i128 = multiplicacao(x, y);
+    println!("{} * {} = {}", x, y, mult);
+
+    let idade:u8 = 9;
+    let faixa_etaria:String = verificar_idade(idade);
+    println!("Faixa etária: {}", faixa_etaria);
+
+    repeticoes(5);
 }
 
 pub fn types(){
     println!("| Types |\n");
     let variavel:i32 = 100;
-    let bytes_var = std::mem::size_of_val(&variavel);
+    let bytes_var:usize = std::mem::size_of_val(&variavel);
     println!("i32 | {} | {} Bytes | {} Bits", variavel, bytes_var, bytes_var * 8);
     
     let decimal:f32 = 2.5;
-    let bytes_decimal = std::mem::size_of_val(&decimal);
+    let bytes_decimal:usize = std::mem::size_of_val(&decimal);
     println!("f32 | {} | {} Bytes | {} Bits", decimal, bytes_decimal, bytes_decimal * 8);
     
     let booleano:bool = true;
-    let bytes_booleano = std::mem::size_of_val(&booleano);
+    let bytes_booleano:usize = std::mem::size_of_val(&booleano);
     println!("bool | {} | {} Bytes", booleano, bytes_booleano);
     
     let letra:char = 'a';
-    let bytes_letra = std::mem::size_of_val(&letra);
-    println!("char | {} | {} Bytes | {} bits\n", letra, bytes_letra, bytes_letra * 8);
+    let bytes_letra:usize = std::mem::size_of_val(&letra);
+    println!("char | {} | {} Bytes | {} bits", letra, bytes_letra, bytes_letra * 8);
     
-    println!("PI = {}\n", PI);
+    //println!("PI = {}\n", PI);
+
     unsafe{
         println!("Global = {}", GLOBAL);
     }
 }
 
 pub fn hello_world(nome:&str) -> String{
-    println!("| Hi! |\n");
     if !nome.is_empty() {
         return format!("Hello World, {}!\n", nome);
     }
-    
-    format!("Hello World!\n")
+
+    "Hello World!\n".to_string()
 }
 
-pub fn soma(x:i128, y:i128) -> String {
-    println!("| Soma |\n");
-    format!("{} + {} = {}", x, y, x + y)
+pub fn soma(x:i128, y:i128) -> i128 {
+    println!("| Soma |");
+    let soma:i128 = x + y;
+
+    soma
+}
+
+pub fn multiplicacao(x:i128, y:i128) -> i128 {
+    println!("| Multiplicação |");
+    let mult:i128 = x * y;
+
+    mult
+}
+
+pub fn verificar_idade(idade:u8) -> String{
+    let faixa_etaria:String= if idade >= 18 { "Adulto" }else if idade >= 10 && idade < 18{ "Adolescente" }else{ "Criança" }.to_string();
+
+    faixa_etaria
+}
+
+pub fn repeticoes(multiplicador:u8){
+    let mut contador:u8 = 0;
+
+    println!("| Tabela de {} |", multiplicador);
+
+    //while
+    while contador < 10{
+        contador += 1;
+
+        if contador == 5{
+           continue;
+        }
+
+        let resultado:u8 = multiplicador * contador;
+        println!("{} * {} = {}", multiplicador, contador, resultado);
+    }
+
+    //loop = while true
+    contador = 0;
+    loop {
+        contador += 1;
+        let resultado:u8 = multiplicador * contador;
+        println!("{} * {} = {}", multiplicador, contador, resultado);
+
+        if contador == 10{
+            break;
+        }
+    }
+
+    //for range
+    for i in 1..11{
+        println!("Index: {}", i);
+    }
+
 }
